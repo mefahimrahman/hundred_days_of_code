a145c13540ed27f3f9e1980ee35c2925e24ddc04
raf-wtag <fahim.rahman@welldev.io>
2022-03-31 12:48:25 +0600

    Empty Project Initiate

---
+++ Project 2/AdvancedTipCalculator/AdvancedTipCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AdvancedTipCalculator
+//
+//  Created by Fahim Rahman on 31/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
