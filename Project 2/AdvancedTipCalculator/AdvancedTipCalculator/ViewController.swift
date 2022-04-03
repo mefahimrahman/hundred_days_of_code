7927aea9e0ec5b85128f5da48b3c2aaf38baecb1
raf-wtag <fahim.rahman@welldev.io>
2022-04-03 17:43:12 +0600

    Project Created

--- Project 2/AdvancedTipCalculator/AdvancedTipCalculator/ViewController.swift
+++ Project 2/AdvancedTipCalculator/AdvancedTipCalculator/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var Button1: UIButton!
+    @IBOutlet var Button2: UIButton!
+    @IBOutlet var Button3: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        Button1.title(for: UIControl.State.highlighted)
+    }
+    
+    @IBAction func Button1Pressed(_ sender: Any) {
+        print("Button 1 Pressed")
+        
     }
 
-
+    @IBAction func Button2Pressed(_ sender: Any, forEvent event: UIEvent) {
+        print("Button 2 Pressed sender -> \(sender) and event \(event)")
+    }
+    
+    @IBAction func Button3Pressed(_ sender: Any, forEvent event: UIEvent) {
+        print("Button 3 Pressed sender -> \(sender) and event \(event)")
+    }
 }
 
---
+++ Project 2/CustomContactApp/CustomContactApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomContactApp
+//
+//  Created by Fahim Rahman on 3/4/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
